f9a4d1b941bab6b5f76cea78f9ac5cb596c6d717
luiso132413 <loviedog@miumg.edu.gt>
2025-04-25 20:56:06 -0600

    test7

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 
-const cliente = require('../controllers/cliente.controller');
+const cliente = require('../controllers/cliente.controller.js');
 
 //rutas del cliente
 router.post('/api/cliente/create', cliente.createCliente);
